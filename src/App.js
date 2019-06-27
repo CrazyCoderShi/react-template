e9a7f750f3804e9f6c70de79480cc515bc82ee4c
天南 <crazycoderss@gmail.com>
2019-06-27 20:18:17 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { PrimaryButton, ActionButton } from "office-ui-fabric-react/lib/Button"
+import { FluentCustomizations } from "@uifabric/fluent-theme"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <PrimaryButton text="标题" onClick={() => {}} primary />
+        <ActionButton text="测试" />
+      </header>
+    </div>
+  )
+}
+
+export default App
